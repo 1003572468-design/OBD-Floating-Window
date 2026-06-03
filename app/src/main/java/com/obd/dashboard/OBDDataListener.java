b03b904b942d7f56c4e9c3394da921578da4626d
1003572468-design <1003572468@qq.com>
2026-06-03 21:04:23 +0800

    Delete app/src/main/java/com/obd/dashboard directory

--- app/src/main/java/com/obd/dashboard/OBDDataListener.java
+++
@@ -1,7 +0,0 @@
-package com.obd.dashboard;
-
-public interface OBDDataListener {
-    void onDataReceived(String data);
-    void onConnectionChanged(boolean connected);
-    void onError(String error);
-}
