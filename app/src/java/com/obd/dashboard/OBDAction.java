55cfa76e873641c2e3a01a7cd874198205781383
1003572468-design <1003572468@qq.com>
2026-06-03 21:05:14 +0800

    Create OBDAction.java

---
+++ app/src/java/com/obd/dashboard/OBDAction.java
@@ -0,0 +1,10 @@
+package com.obd.dashboard;
+
+public class OBDAction {
+    public static final String ACTION_OBDBUNDLE = "com.cpsdna.obdport.data";
+    public static final String ACTION_OBD_FIRE = "com.cpsdna.obdport.fire";
+    public static final String OBD_STATUS_CONNECTED = "OBD_STATUS_CONNECTED";
+    public static final String OBD_STATUS_ERROR = "OBD_STATUS_ERROR";
+    public static final String OBD_VIN = "OBD_VIN";
+    public static final String KEY_FIRE = "fire";
+}
