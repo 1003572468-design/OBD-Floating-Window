f1fd745aad79af57ea7091bd6c27cdcd4c38b8f9
1003572468-design <1003572468@qq.com>
2026-06-04 12:47:15 +0800

    Create AIObdDataMessage.java

---
+++ app/src/main/java/com/example/obdparser/AIObdDataMessage.java
@@ -0,0 +1,10 @@
+package com.obd.dashboard;
+
+public class AIObdDataMessage {
+    public static final String OBD_REAL_MESSAGE_FILTER = "com.ileja.aicar.obd.real";
+    public static final String OBD_HISTORY_MESSAGE_FILTER = "com.ileja.aicar.obd.history";
+    public static final String OBD_DRIVING_MESSAGE_FILTER = "com.ileja.aicar.obd.driving";
+    public static final String OBD_ERROR_MESSAGE_FILTER = "com.ileja.aicar.obd.error";
+    public static final String OBD_FUEL_MESSAGE_FILTER = "com.ileja.aicar.obd.fuel";
+    public static final String OBD_STATUS_MESSAGE_FILTER = "com.ileja.aicar.obd.status";
+}
